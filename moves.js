8f24f2a4600b47de86533cd3ffa6870e2b187be6
jerryluo <jerryluogd@gmail.com>
2025-08-11 22:17:55 -0400

    Did most of it

---
+++ moves.js
@@ -0,0 +1,247 @@
+function rookPossibleMoves(color, position, boardPos) {
+    let allyPiece;
+    let enemyPiece;
+    if (color === "W") {
+        allyPiece = "RBNKQP";
+        enemyPiece = "rbnkqp";
+    } else {
+        enemyPiece = "RBNKQP";
+        allyPiece = "rbnkqp";
+    }
+
+    let positionIndexList = [];
+
+    const directions = [1, -1, -8, 8];
+    let r = Math.floor(position / 8);
+    let c = position % 8;
+
+    for (let i = 0; i < directions.length; i++) {
+        let j = 1;
+        while (true) {
+            let potentialIndex = position + directions[i] * j;
+            let potentialRow = Math.floor(potentialIndex / 8);
+            let potentialCol = potentialIndex % 8;
+
+            if (
+                potentialRow < 0 ||
+                potentialRow >= 8 ||
+                potentialCol < 0 ||
+                potentialCol >= 8
+            )
+                break;
+
+            if (
+                (directions[i] == 1 || directions[i] == -1) &&
+                potentialRow != r
+            )
+                break;
+
+            if (allyPiece.includes(ChessBoardPosition[potentialIndex])) break;
+            positionIndexList.push(potentialIndex);
+            if (enemyPiece.includes(ChessBoardPosition[potentialIndex])) break;
+            j++;
+        }
+    }
+    console.log(positionIndexList);
+    return positionIndexList;
+}
+
+function bishopPossibleMoves(color, position, boardPos) {
+    let allyPiece;
+    let enemyPiece;
+    if (color === "W") {
+        allyPiece = "RBNKQP";
+        enemyPiece = "rbnkqp";
+    } else {
+        enemyPiece = "RBNKQP";
+        allyPiece = "rbnkqp";
+    }
+
+    let positionIndexList = [];
+
+    const directions = [9, 7, -9, -7];
+    let r = Math.floor(position / 8);
+    let c = position % 8;
+
+    for (let i = 0; i < directions.length; i++) {
+        let j = 1;
+        while (true) {
+            let potentialIndex = position + directions[i] * j;
+            let potentialRow = Math.floor(potentialIndex / 8);
+            let potentialCol = potentialIndex % 8;
+
+            if (
+                potentialRow < 0 ||
+                potentialRow >= 8 ||
+                potentialCol < 0 ||
+                potentialCol >= 8 ||
+                Math.abs(potentialCol - c) !== j ||
+                Math.abs(potentialRow - r) !== j
+            )
+                break;
+
+            if (allyPiece.includes(ChessBoardPosition[potentialIndex])) break;
+            positionIndexList.push(potentialIndex);
+            if (enemyPiece.includes(ChessBoardPosition[potentialIndex])) break;
+            j++;
+        }
+    }
+    console.log(positionIndexList);
+    return positionIndexList;
+}
+//const directions = [-10,-6,-17, -15, 6, 15, 17, 10];
+function knightPossibleMoves(color, position, boardPos) {
+    let allyPiece;
+    let enemyPiece;
+    if (color === "W") {
+        allyPiece = "RBNKQP";
+        enemyPiece = "rbnkqp";
+    } else {
+        enemyPiece = "RBNKQP";
+        allyPiece = "rbnkqp";
+    }
+
+    let positionIndexList = [];
+
+    const directions = [-10, -6, -17, -15, 6, 15, 17, 10];
+    let r = Math.floor(position / 8);
+    let c = position % 8;
+
+    for (let i = 0; i < directions.length; i++) {
+        let potentialIndex = position + directions[i];
+        let potentialRow = Math.floor(potentialIndex / 8);
+        let potentialCol = potentialIndex % 8;
+
+        if (
+            potentialRow < 0 ||
+            potentialRow >= 8 ||
+            potentialCol < 0 ||
+            potentialCol >= 8
+        ) {
+            continue;
+        }
+
+        let rowDiff = Math.abs(potentialRow - r);
+        let colDiff = Math.abs(potentialCol - c);
+
+        if (
+            !(
+                (rowDiff === 2 && colDiff === 1) ||
+                (rowDiff === 1 && colDiff === 2)
+            )
+        ) {
+            continue;
+        }
+
+        if (allyPiece.includes(ChessBoardPosition[potentialIndex])) continue;
+        positionIndexList.push(potentialIndex);
+    }
+    console.log(positionIndexList);
+    return positionIndexList;
+}
+function kingPossibleMoves(color, position, boardPos) {
+    let allyPiece;
+    let enemyPiece;
+    if (color === "W") {
+        allyPiece = "RBNKQP";
+        enemyPiece = "rbnkqp";
+    } else {
+        enemyPiece = "RBNKQP";
+        allyPiece = "rbnkqp";
+    }
+
+    let positionIndexList = [];
+
+    const directions = [9, 7, -9, -7, 1, -1, 8, -8];
+    let r = Math.floor(position / 8);
+    let c = position % 8;
+
+    for (let i = 0; i < directions.length; i++) {
+        let potentialIndex = position + directions[i];
+        let potentialRow = Math.floor(potentialIndex / 8);
+        let potentialCol = potentialIndex % 8;
+
+        if (
+            potentialRow < 0 ||
+            potentialRow >= 8 ||
+            potentialCol < 0 ||
+            potentialCol >= 8
+        )
+            continue;
+
+        if (
+            [9, 7, -9, -7].includes(directions[i]) &&
+            (Math.abs(potentialCol - c) !== 1 ||
+                Math.abs(potentialRow - r) !== 1)
+        )
+            continue;
+
+        if ((directions[i] == 1 || directions[i] == -1) && potentialRow != r)
+            continue;
+
+        if (allyPiece.includes(ChessBoardPosition[potentialIndex])) continue;
+        positionIndexList.push(potentialIndex);
+        if (enemyPiece.includes(ChessBoardPosition[potentialIndex])) continue;
+    }
+    console.log(positionIndexList);
+    return positionIndexList;
+}
+
+function queenPossibleMoves(color, position, boardPos) {
+    let allyPiece;
+    let enemyPiece;
+    if (color === "W") {
+        allyPiece = "RBNKQP";
+        enemyPiece = "rbnkqp";
+    } else {
+        enemyPiece = "RBNKQP";
+        allyPiece = "rbnkqp";
+    }
+
+    let positionIndexList = [];
+
+    const directions = [9, 7, -9, -7, 1, -1, 8, -8];
+    let r = Math.floor(position / 8);
+    let c = position % 8;
+
+    for (let i = 0; i < directions.length; i++) {
+        let j = 1;
+        while (true) {
+            let potentialIndex = position + directions[i] * j;
+            let potentialRow = Math.floor(potentialIndex / 8);
+            let potentialCol = potentialIndex % 8;
+
+            if (
+                potentialRow < 0 ||
+                potentialRow >= 8 ||
+                potentialCol < 0 ||
+                potentialCol >= 8
+            )
+                break;
+
+            if (
+                [9, 7, -9, -7].includes(directions[i]) &&
+                (Math.abs(potentialCol - c) !== j ||
+                    Math.abs(potentialRow - r) !== j)
+            )
+                break;
+
+            if (
+                (directions[i] == 1 || directions[i] == -1) &&
+                potentialRow != r
+            )
+                break;
+
+            if (allyPiece.includes(ChessBoardPosition[potentialIndex])) break;
+            positionIndexList.push(potentialIndex);
+            if (enemyPiece.includes(ChessBoardPosition[potentialIndex])) break;
+            j++;
+        }
+    }
+    console.log(positionIndexList);
+    return positionIndexList;
+}
+
+function pawnPossibleMoves(color, position, boardPos) {}
+
+function pawnPromotion(color, position, boardPos) {}
---
+++ script.js
@@ -0,0 +1,127 @@
+let ChessBoardPosition = `N0p0P1QbKR00n0kqPpp0000BqN0rbRp000n0B0P000RP0K000rNBQ0p`;
+//rnbqkbnrpppppppp00000000000000000000000000000000PPPPPPPPRNBQKBNR
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const vh = window.innerHeight / 100;
+const squareSizePx = 11 * vh;
+const WhitePiece = "RNBKQP";
+const BlackPiece = "rnbkqp";
+
+function createSquare() {
+    const cont = document.querySelector(".main-cont");
+    for (let i = 0; i < 64; i++) {
+        let r = Math.floor(i / 8);
+        let c = i % 8;
+        let div = document.createElement("div");
+        div.classList.add("square");
+        div.id = `${i}`;
+        div.addEventListener("click", handleClick);
+        if ((r + c) % 2 === 0) div.style.backgroundColor = "#DEE3E6";
+        else div.style.backgroundColor = "#8CA2AD";
+        cont.appendChild(div);
+    }
+}
+
+let highlighted = null;
+
+function handleClick(e) {
+    const divId = e.target.id;
+    if (highlighted !== null) {
+        const prev = document.getElementById(highlighted);
+        prev.style.border = "";
+    }
+    e.target.style.border = "3px yellow solid";
+
+    highlighted = divId;
+    if (WhitePiece.includes(ChessBoardPosition[divId])) {
+        possibleMoves(ChessBoardPosition[divId], parseInt(divId));
+    }
+}
+
+function possibleMoves(piece, position) {
+    switch (piece) {
+        case "r":
+            return rookPossibleMoves("B", position, ChessBoardPosition);
+        case "n":
+            return knightPossibleMoves("B", position, ChessBoardPosition);
+        case "b":
+            return bishopPossibleMoves("B", position, ChessBoardPosition);
+        case "q":
+            return queenPossibleMoves("B", position, ChessBoardPosition);
+        case "k":
+            return kingPossibleMoves("B", position, ChessBoardPosition);
+        case "p":
+            return pawnPossibleMoves("B", position, ChessBoardPosition);
+        case "R":
+            return rookPossibleMoves("W", position, ChessBoardPosition);
+        case "N":
+            return knightPossibleMoves("W", position, ChessBoardPosition);
+        case "B":
+            return bishopPossibleMoves("W", position, ChessBoardPosition);
+        case "Q":
+            return queenPossibleMoves("W", position, ChessBoardPosition);
+        case "K":
+            return kingPossibleMoves("W", position, ChessBoardPosition);
+        case "P":
+            return pawnPossibleMoves("W", position, ChessBoardPosition);
+        default:
+            img.src = "";
+    }
+}
+
+function positionUpdate(ChessBoardPosition) {
+    for (let i = 0; i < ChessBoardPosition.length; i++) {
+        const img = new Image();
+        switch (ChessBoardPosition[i]) {
+            case "r":
+                img.src = "piece/Chess_rdt60.png";
+                break;
+            case "n":
+                img.src = "piece/Chess_ndt60.png";
+                break;
+            case "b":
+                img.src = "piece/Chess_bdt60.png";
+                break;
+            case "q":
+                img.src = "piece/Chess_qdt60.png";
+                break;
+            case "k":
+                img.src = "piece/Chess_kdt60.png";
+                break;
+            case "p":
+                img.src = "piece/Chess_pdt60.png";
+                break;
+            case "R":
+                img.src = "piece/Chess_rlt60.png";
+                break;
+            case "N":
+                img.src = "piece/Chess_nlt60.png";
+                break;
+            case "B":
+                img.src = "piece/Chess_blt60.png";
+                break;
+            case "Q":
+                img.src = "piece/Chess_qlt60.png";
+                break;
+            case "K":
+                img.src = "piece/Chess_klt60.png";
+                break;
+            case "P":
+                img.src = "piece/Chess_plt60.png";
+                break;
+            default:
+                img.src = "";
+        }
+
+        img.onload = function () {
+            r = Math.floor(i / 8);
+            c = i % 8;
+            if (img.src != "") {
+                ctx.drawImage(img, c * 37.5, r * 18.9, 37.5, 18.9);
+            }
+        };
+    }
+}
+
+createSquare();
+positionUpdate(ChessBoardPosition);
