fce1ba68c0534a335273069f01fc6663defe91f7
jerryluo01 <jerryluogd@gmail.com>
2025-09-06 19:25:26 -0400

    a

--- ai_server.py
+++ ai_server.py
@@ -2,6 +2,7 @@ import math
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import ai
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -21,4 +22,5 @@ def get_ai_move():
     return jsonify({"move": move})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
